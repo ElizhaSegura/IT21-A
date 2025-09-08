5f8825bbe9f7716213b7aae2c43089ff95d5f30a
ElizhaSegura <elizhaalthea@gmail.com>
2025-09-08 21:21:31 +0800

    Added logic and computation for factorial.js

--- factorial.js
+++ factorial.js
@@ -5,19 +5,41 @@ const rl = readline.createInterface({
     output:process.stdout
 })
 
+
+    console.log("Factorial Application\n");
+
 console.log("1. Say Hello");
 console.log("2. Compute Factorial");
 console.log("3. Exit");
 
-rl.question("Enter your choice (1-3)", choice =>{
+rl.question("Enter your choice (1-3): ", choice =>{
+
     if(choice==="1"){
         console.log("Hello There!");
     } else if (choice==="2"){
-        console.log("Factorial");
+        console.log("Factorial\n");
+        
+        rl.question("Enter a number for factorial: ", (numStr) =>{
+            let num = parseInt(numStr);
+            
+            if(isNaN(num) || num<0){
+                console.log("Please enter a non-negative integer.");
+            } else {
+                
+                let fact = 1;
+
+                for (let i = 1; i <= num ; i++){
+                    fact *=i;
+                }
+                console.log("The factorial of "+ num + " is "+ fact);
+            }
+
+        });
+
     }else if(choice==="3"){
-        console.log("Exit");
+         console.log("Exit");
     }else{
-        console.log("Invalid choice. Please Try Again");
+        console.log("Invalid choice. Please Try Again.");
     }
     
 })
