725c6fca2c2ba92039a813f159cea2ccfc325e78
ElizhaSegura <elizhaalthea@gmail.com>
2025-09-15 21:56:31 +0800

    Added Hero class and initial instance

---
+++ hero.js
@@ -0,0 +1,16 @@
+class Hero{
+    #health;
+
+    constructor (name, health, attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+    }
+
+    getHealth(){
+        return this.#health;
+    }
+}
+    const hero = new Hero("Thorin", 100, 10);
+
+    console.log(hero.getHealth());
