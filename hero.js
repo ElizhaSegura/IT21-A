8308cee7ec56dcf176f24e83e78af0d7a824216a
ElizhaSegura <elizhaalthea@gmail.com>
2025-09-15 22:14:07 +0800

    Added wizard subclass and updated object instance

--- hero.js
+++ hero.js
@@ -1,16 +1,26 @@
-class Hero{
+class Hero {
     #health;
 
-    constructor (name, health, attack){
+    constructor(name, health, attack) {
         this.name = name;
         this.#health = health;
         this.attack = attack;
     }
 
-    getHealth(){
+    getHealth() {
         return this.#health;
     }
 }
-    const hero = new Hero("Thorin", 100, 10);
+// const hero = new Hero("Thorin", 100, 10);
 
-    console.log(hero.getHealth());
+class Wizard extends Hero {
+    useAbility() {
+        console.log(`${this.name} casts... ilLuSi0n bLaAasT!!!`);
+    }
+}
+
+//console.log(hero.getHealth());
+
+const wizard = new Wizard("Grand Magus", 100, 10);
+wizard.useAbility();
+
