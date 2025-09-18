f7e6057a313b14f2601e8d99bc46b44937f5a2fd
ElizhaSegura <elizhaalthea@gmail.com>
2025-09-18 08:46:46 +0800

    Added item class for composition

--- hero.js
+++ hero.js
@@ -5,10 +5,19 @@ class Hero {
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     }
 
     getHealth() {
         return this.#health;
+    }
+
+    getName(){
+        console.log(this.name);
+    }
+
+    getAttack(){
+        console.log(this.attack);
     }
 
     getStats() {
@@ -17,6 +26,15 @@ class Hero {
         console.log("Health: " + this.#health);
         console.log("Attack: " + this.attack);
     }
+
+    addItem(item){
+        this.items.push(item);
+   }
+
+   totalAttack(){
+    return this.attack +this.items.reduce((sum, i) =>sum+i.bonusAttack,0);
+   }
+
 }
 // const hero = new Hero("Thorin", 100, 10);
 
@@ -39,14 +57,39 @@ class Witch extends Hero {
     }
 }
 
+class Item{
+    constructor(name, bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
+}
+
+const staff = new Item("Staff", 5);
+const broom = new Item("Broom", 3);
 
 const Magus = new Wizard("Magus", 100, 10);
-Magus.useAbility();
-Magus.getStats();
 
-const Elphaba= new Witch("Elphaba", 80, 8, 50);
-Elphaba.useAbility(); 
-Elphaba.getStats();
+//Magus.getStats();
+Magus.getName();
+Magus.getAttack();
+
+Magus.addItem(staff);
+console.log(Magus.totalAttack());
+Magus.addItem(staff);
+console.log(Magus.totalAttack());
+Magus.addItem(staff);
+console.log(Magus.totalAttack());
+Magus.addItem(staff);
+console.log(Magus.totalAttack());
+
+
+//const Magus = new Wizard("Magus", 100, 10);
+//Magus.useAbility();
+//Magus.getStats();
+
+//const Elphaba= new Witch("Elphaba", 80, 8, 50);
+//Elphaba.useAbility(); 
+//Elphaba.getStats();
 
 
 function performAbility(hero) {
@@ -55,7 +98,7 @@ function performAbility(hero) {
 }
 
 
-performAbility(Magus);
+//performAbility(Magus);
 
-performAbility(Elphaba);
+//performAbility(Elphaba);
 
