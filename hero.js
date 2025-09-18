bf7844947ff27bf045132e60d91d17e2a97bff8b
ElizhaSegura <elizhaalthea@gmail.com>
2025-09-18 08:12:00 +0800

    Added perform ability as abstraction sample

--- hero.js
+++ hero.js
@@ -10,6 +10,13 @@ class Hero {
     getHealth() {
         return this.#health;
     }
+
+    getStats() {
+        console.log(" \n ");
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.#health);
+        console.log("Attack: " + this.attack);
+    }
 }
 // const hero = new Hero("Thorin", 100, 10);
 
@@ -21,20 +28,34 @@ class Wizard extends Hero {
 
 //console.log(hero.getHealth());
 
-class Witch extends Hero{
-    constructor(name,health,attack,mana){
-        super(name,health,attack);
+class Witch extends Hero {
+    constructor(name, health, attack, mana) {
+        super(name, health, attack);
         this.mana = mana;
-}
-  
-    useAbility(){
+    }
+
+    useAbility() {
         console.log(`${this.name} roars Eeeemeraaaald VooOooorrrtex!!!`);
     }
 }
 
-const wizard = new Wizard("Grand Magus", 100, 10);
-wizard.useAbility();
 
-const witch = new Witch("Elphaba", 80, 8, 50);
-witch.useAbility();
+const Magus = new Wizard("Magus", 100, 10);
+Magus.useAbility();
+Magus.getStats();
+
+const Elphaba= new Witch("Elphaba", 80, 8, 50);
+Elphaba.useAbility(); 
+Elphaba.getStats();
+
+
+function performAbility(hero) {
+    console.log("\n");
+    hero.useAbility();
+}
+
+
+performAbility(Magus);
+
+performAbility(Elphaba);
 
